e6b67526e60be31d32ac617a78f6055aa09379a4
Sean Ankenbruck <smankenb@gmail.com>
2014-04-15 21:14:04 -0400

    added validation tests

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,13 @@
 class Recipe < ActiveRecord::Base
+
+	validates :name, presence: true, length: {minimum: 2}
+
+
+	def minutes_total
+		if (minutes_to_prepare)
+			minutes_to_prepare + minutes_to_cook
+		else
+			minutes_to_cook
+		end
+	end
 end
